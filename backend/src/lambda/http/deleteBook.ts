3a34508489ddb0bed08a9573cf0b9a03c97da985
realnitinworks <realnitinworks@gmail.com>
2020-12-29 17:14:59 +0530

    feat: Implement Lambda to delete a book

--- backend/src/lambda/http/deleteBook.ts
+++ backend/src/lambda/http/deleteBook.ts
@@ -1,17 +1,31 @@
 import 'source-map-support/register'
-
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import * as AWS from 'aws-sdk'
+
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+const booksTable = process.env.BOOKS_TABLE;
+
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  const bookId = event.pathParameters.bookId
+  console.log("Processing event: ", event);
+  
+  const userId = "123";
+  const bookId = event.pathParameters.bookId;
+
+  await docClient.delete({
+    TableName: booksTable,
+    Key: {
+      userId,
+      bookId
+    }
+  }).promise();
 
   return {
     statusCode: 200,
     headers: {
       'Access-Control-Allow-Origin': '*'
     },
-    body: JSON.stringify({
-      bookId
-    })
+    body: ""
   }
 }
