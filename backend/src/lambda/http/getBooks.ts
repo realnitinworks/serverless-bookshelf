7e762487d0219a82fe9b9280b1260e10bca78743
realnitinworks <realnitinworks@gmail.com>
2020-12-29 07:14:05 +0530

    feat: Implement Lambda for getting books for a user

--- backend/src/lambda/http/getBooks.ts
+++ backend/src/lambda/http/getBooks.ts
@@ -1,9 +1,27 @@
 import 'source-map-support/register'
-
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import * as AWS from 'aws-sdk'
+
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+const booksTable = process.env.BOOKS_TABLE;
+const createdAtIndex = process.env.CREATED_AT_INDEX;
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   console.log("Processing event: ", event);
+
+  const userId = "123";
+  const result = await docClient.query({
+    TableName: booksTable,
+    IndexName: createdAtIndex,
+    KeyConditionExpression: 'userId = :userId',
+    ExpressionAttributeValues: {
+      ':userId': userId
+    },
+    ScanIndexForward: false
+  }).promise();
+
+  const items = result.Items;
 
   return {
     statusCode: 200,
@@ -11,7 +29,7 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
       'Access-Control-Allow-Origin': '*'
     },
     body: JSON.stringify({
-      items: []
+      items
     })
   }
 }
